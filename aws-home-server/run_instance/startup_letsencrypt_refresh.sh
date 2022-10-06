26781ba3d5b14c5cc26bce9f8e62317710f07108
nate <nathan@tippy.name>
2022-10-05 22:36:02 -0500

    certbot is dumb and we must move the files

--- aws-home-server/run_instance/startup_letsencrypt_refresh.sh
+++ aws-home-server/run_instance/startup_letsencrypt_refresh.sh
@@ -9,6 +9,12 @@ if [ -f "/var/archive/letsencrypt.zip" ]; then
       echo "cert is older than 80 days, time to renew"
       sudo cd /var/archive
       sudo certbot certonly --standalone --register-unsafely-without-email -d ${TF-DOMAIN}
+
+      sudo rm -fR /etc/letsencrypt/live/old
+      sudo mv /etc/letsencrypt/live/${TF-DOMAIN} /etc/letsencrypt/live/old
+      export NEW_FOLDER="$(sudo ls /etc/letsencrypt/live -ltr | tail -n 1 | awk '{print $9}')"
+      sudo mv /etc/letsencrypt/live/${NEW_FOLDER} /etc/letsencrypt/live/${TF-DOMAIN}
+
       sudo zip /var/archive/letsencrypt.zip /etc/letsencrypt/* -r 
     else
       echo "use the stored cert"
